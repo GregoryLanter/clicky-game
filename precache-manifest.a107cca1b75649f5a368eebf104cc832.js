b2e8fea9919094bc3bcffb665cd81a36e126d1cd
Greg Lanter <dohnut22.gl@gmail.com>
2019-07-27 19:17:36 -0700

    Updates

---
+++ precache-manifest.a107cca1b75649f5a368eebf104cc832.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d021220889e891ea0e9561ef7734427",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "79c442c129f0338f0c1b",
+    "url": "/clicky-game/static/css/main.dfca3b62.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/clicky-game/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "79c442c129f0338f0c1b",
+    "url": "/clicky-game/static/js/main.3c107fdd.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
